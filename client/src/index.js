46842ce79b92b26af8be4bbc9cf6a06cfde866f6
Mateusz Bober <bober.mateusz17@gmail.com>
2022-04-23 23:46:10 +0100

    Small Fixes

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -9,26 +9,25 @@ app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-app.post("/post-test", (req, res) => {
-  let data = { name: req.body.name };
-  console.log(req.headers);
-  console.log("got body:", req.body, res.body);
-  res.sendStatus(200);
-});
-
 const port = process.env.PORT || 5000;
 //Start the server
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "client ", "build", "index.html"));
+});
+
 app.listen(port, () => console.log(`server started on port 5000`));
 
-app.get("/output", (req, res) => {
-  const output = [{ data: "Some output" }];
-  res.send(output);
-});
+// app.get("/output", (req, res) => {
+//   const output = [{ data: "Some output" }];
+//   res.send(output);
+// });
 
-app.get("/", (req, res) => {
-  const customers = [{ data: "Hello World" }];
-  res.json(customers);
-});
+// app.post("/post-test", (req, res) => {
+//   let data = { name: req.body.name };
+//   console.log(req.headers);
+//   console.log("got body:", req.body, res.body);
+//   res.sendStatus(200);
+// });
