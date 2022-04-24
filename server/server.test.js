69829c6cca833cccd998293e05a16c772271bbcd
Mateusz Bober <bober.mateusz17@gmail.com>
2022-04-24 22:14:54 +0100

    Final commit

---
+++ server/server.test.js
@@ -0,0 +1,14 @@
+// import supertest from "supertest";
+// import server from "./server.js";
+
+// // describe("create /User", () => {
+// //   describe("Given Username, Password and Email", () => {
+// //     test("should respond with 200 status code", async () => {
+// //       const response = await request(app)
+// //         .get("/add-user")
+// //         .send()
+// //         .expect(response.statusCode)
+// //         .toBe(200);
+// //     });
+// //   });
+// // });
